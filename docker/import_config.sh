b59635fd4f85dfd6b6d3196e3d0c770a635c1e4e
Tenyo Grozev <tenyo.grozev@yale.edu>
2018-10-26 15:16:39 -0400

    fixup

--- docker/import_config.sh
+++ docker/import_config.sh
@@ -11,7 +11,7 @@ if [ -n "$S3URL" ]; then
     exit 1
   fi
   aws --region us-east-1 s3 cp ${S3URL} ./config.encrypted
-  aws --region us-east-1 kms decrypt --ciphertext-blob fileb://config.encrypted --output text --query Plaintext | base64 -d > config.json
+  aws --region us-east-1 kms decrypt --ciphertext-blob fileb://config.encrypted --output text --query Plaintext | base64 -d > config/config.json
   rm -f config.encrypted
 else
   echo "ERROR: S3URL variable not set!"
