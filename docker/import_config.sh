8bfc774b37bb8396543821642aec87c50130d672
Tenyo Grozev <tenyo.grozev@yale.edu>
2020-06-25 11:46:07 -0400

    fixup

--- docker/import_config.sh
+++ docker/import_config.sh
@@ -5,15 +5,13 @@
 
 if [ -n "$SSMPATH" ]; then
   echo "Getting config file from SSM Parameter Store (${SSMPATH}) ..."
-  aws --version
+  deco version
   if [[ $? -ne 0 ]]; then
-    echo "ERROR: aws-cli not found!"
+    echo "ERROR: deco not found!"
     exit 1
   fi
-  aws --region us-east-1 ssm get-parameter --name "${SSMPATH}" --with-decryption --output text --query Parameter.Value | base64 -d > deco-config.json
-  deco validate deco-config.json || exit 1
-  deco run deco-config.json
-  rm -f deco-config.json config.encrypted
+  deco validate -e ssm://${SSMPATH} || exit 1
+  deco run -e ssm://${SSMPATH}
 else
   echo "ERROR: SSMPATH variable not set!"
   exit 1
